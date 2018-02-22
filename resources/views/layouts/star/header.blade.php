caf1de45c2245d7ffc833094ab28a30f213d3d32
Hong Jin-Seob <hamunstorys@gmail.com>
2018-02-22 19:41:27 +0900

    #11 matching files

--- resources/views/layouts/star/header.blade.php
+++ resources/views/layouts/star/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <div class="top">
         <div class="logo"><a href="{{route('star.index')}}"><img
-                        src="{{asset('assets/star/img/sj_logo.svg')}}"></a><span>SJ COMPANY</span> 아티스트 데이터베이스
+                        src="{{asset('assets/star/img/logo.svg')}}"></a><span>SJ COMPANY</span> 아티스트 데이터베이스
         </div>
         <div class="logout"><a href="{{route('star.artist.create')}}">
                 <button>등록</button>
