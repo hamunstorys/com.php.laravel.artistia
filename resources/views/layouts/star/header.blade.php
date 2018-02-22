10a792638f9591f964eee97144efe88c2526b84c
Hong Jin-Seob <hamunstorys@gmail.com>
2018-02-22 10:44:14 +0900

    #4 add brand log's link, mapping search icon. by Hong Jin-Seob

--- resources/views/layouts/star/header.blade.php
+++ resources/views/layouts/star/header.blade.php
@@ -1,6 +1,8 @@
 <header>
     <div class="top">
-        <div class="logo"><img src="{{asset('assets/img/star/sj_logo.svg')}}"><span>SJ COMPANY</span>아티스트 데이터베이스</div>
+        <div class="logo"><a href="{{route('star.index')}}"><img
+                        src="{{asset('assets/img/star/sj_logo.svg')}}"></a><span>SJ COMPANY</span> 아티스트 데이터베이스
+        </div>
         <div class="logout"><a href="{{route('star.artist.create')}}">
                 <button>등록</button>
             </a>
