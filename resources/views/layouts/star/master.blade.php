dbbf37aeb944b88b11d20a071703534134e4fea0
Hong Jin-Seob <hamunstorys@gmail.com>
2018-02-22 11:22:03 +0900

    #5 add input autocomplte off

--- resources/views/layouts/star/master.blade.php
+++ resources/views/layouts/star/master.blade.php
@@ -6,7 +6,7 @@
         {{csrf_field()}}
         <div class="search_header">
             <div class="search_wrap">
-                <input class="form-search" name="query" placeholder="검색어를 입력해 주세요">
+                <input class="form-search" name="query" placeholder="검색어를 입력해 주세요" >
                 <button class="btn_search" type="submit"></button>
             </div>
         </div>
