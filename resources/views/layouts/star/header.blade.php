b7e188d02e7aeb1de0fd99aa2af62d72d5f209d7
Hong Jin-Seob <hamunstorys@gmail.com>
2018-02-23 13:29:11 +0900

    2018/02/23 update css, search feature

--- resources/views/layouts/star/header.blade.php
+++ resources/views/layouts/star/header.blade.php
@@ -4,13 +4,13 @@
                         src="{{asset('assets/star/img/logo.svg')}}"></a><span>SJ COMPANY</span> 아티스트 데이터베이스
         </div>
         <div class="logout">
-            <form action="{{route('star.artist.create')}}">
-                <button type="submit">등록</button>
+            <form action="#">
+                <button type="submit">로그아웃</button>
             </form>
         </div>
         <div class="logout">
-            <form action="#">
-                <button type="submit">로그아웃</button>
+            <form action="{{route('star.artist.create')}}">
+                <button type="submit">등록</button>
             </form>
         </div>
     </div>
