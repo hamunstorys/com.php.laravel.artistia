35100dd19649c2243f4d30d6121ffe78f95044c3
Hong Jin-Seob <hamunstorys@gmail.com>
2018-02-26 19:40:46 +0900

    로그인 롤백

--- app/Http/Controllers/Star/SessionController.php
+++ app/Http/Controllers/Star/SessionController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Star;
 use App\Http\Controllers\Controller;
 use App\Models\Star\Star_User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class SessionController extends Controller
@@ -23,16 +22,8 @@ class SessionController extends Controller
     public function store(Request $request)
     {
 
-//        $query = DB::table('star_users')->where('email', $request->only('email'))->first();
-//
-//        if (DB::table('star_users')->where('email', $request->only('email'))->first() != null) {
-//
-//        } else {
-//            return redirect(route('star.notification.error', ['message' => '비밀번호가 맞지 않습니다.', 'route' => 'star.session.create']));
-//        }
-
-        if (!auth()->attempt(['email' => $request->only('email'), 'password' => $request->only('password')])) {
-            return redirect(route('star.notification.error', ['message' => '로그인 정보가 맞지 않습니다.', 'route' => 'star.session.create']));
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            return redirect(route('star.notification.error', ['message' => '비밀번호가 맞지 않습니다.', 'route' => 'star.session.create']));
         }
 
         return redirect()->route('star.index');
--- resources/views/star/session/create.blade.php
+++ resources/views/star/session/create.blade.php
@@ -9,7 +9,7 @@
                 <h1>LOGIN</h1>
                 <input type="text" name="email" placeholder="이메일을 입력해주세요" value="{{old('email')}}">
                 <input type="password" name="password" placeholder="비밀번호를 입력해주세요" value="{{old('password')}}">
-                <button type="submit" name="submit" label="로그인하기">로그인</button>
+                <button type="submit" label="로그인하기">로그인</button>
             </div>
         </FORM>
         <div class="copyright">
